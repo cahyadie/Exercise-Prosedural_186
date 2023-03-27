89efe6cf9b276ba71a29451cda984eee4bcd529d
cahyadie <cahyadiilham12@gmail.com>
2023-03-27 09:25:25 +0700

    2

--- Exercise Prosedural_186/Exercise Prosedural_186.cpp
+++ Exercise Prosedural_186/Exercise Prosedural_186.cpp
@@ -5,3 +5,12 @@ double rerata(double a, double b) {
 	return (a + b) / 2;
 }
 
+string status(double rata, double nill) {
+	if (rata >= 70 || nil >= 80)
+		return "lulus";
+	else
+		return "gagal";
+}
+
+
+}
