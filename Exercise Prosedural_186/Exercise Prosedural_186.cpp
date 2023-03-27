22ee26b7f4ac0c2a9ad57827a124462434f21d7c
cahyadie <cahyadiilham12@gmail.com>
2023-03-27 09:38:10 +0700

    3

--- Exercise Prosedural_186/Exercise Prosedural_186.cpp
+++ Exercise Prosedural_186/Exercise Prosedural_186.cpp
@@ -5,12 +5,19 @@ double rerata(double a, double b) {
 	return (a + b) / 2;
 }
 
-string status(double rata, double nill) {
+string status(double rata, double nil) {
 	if (rata >= 70 || nil >= 80)
 		return "lulus";
 	else
 		return "gagal";
 }
 
-
+int main(){
+	double nilM, nilB;
+	cout << "Masukan nilai Matematika = ";
+	cin >> nilM;
+	cout << "Masukan nilai Bahasa Inggris = ";
+	cin >> nilB;
+	cout << "Status kelulusan = " << status(rerata(nilM, nilB),nilM);
+	return 0;
 }
